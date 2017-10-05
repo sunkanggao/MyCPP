53217204542a5843330ab8594394ad2352de8f2f
sunkanggao <sunkanggao1992@gmail.com>
2017-10-05 14:43:44 +0800

    二分查找

--- base/binarySearch.cpp
+++ base/binarySearch.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 #define MAX_SIZE 102
 
+//递归实现
 template<class T>
 int BinarySearch(T a[], const T& x, int n)
 {
@@ -21,6 +22,21 @@ int BinarySearch(T a[], const T& x, int n)
     return -1;
 }
 
+//非递归实现
+template<class T>
+int BinarySearch2(T a[], const T& x, int left, int right)
+{
+    if(left > right)
+        return -1;
+    int mid = (left + right) / 2;
+    if(a[mid] == x)
+        return mid;
+    else if(a[mid] > x)
+        BinarySearch2(a, x, left, mid - 1);
+    else if(a[mid] < x)
+        BinarySearch2(a, x, mid + 1, right);
+}
+
 int main()
 {
     int a[MAX_SIZE];
@@ -30,6 +46,7 @@ int main()
         cin >> a[i];
     cin >> x;
     p = BinarySearch(a, x, len);
+    // p = BinarySearch2(a, x, 0, len - 1);
     if(p == -1)
         cout << "该数不存在！" << endl;
     else
